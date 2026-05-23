f3fcbef5758f1c41e9ce290eadd3010d82b36e80
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-05-23 14:50:01 -0300

    feat(vrr): add vrr=2

--- hypr/custom/general.lua
+++ hypr/custom/general.lua
@@ -4,55 +4,61 @@
 ----- [[ Monitor Configs]] -----
 -- Main monitor config
 hl.monitor({
-    output = "eDP-1",
-    mode = "preferred",
-    position = "auto",
-    scale = "1"
+	output = "eDP-1",
+	mode = "preferred",
+	position = "auto",
+	scale = "1",
 })
 
 -- Secondary HDMI monitor
 hl.monitor({
-    output = "HDMI-A-1",
-    mode = "1920x1080@100.00Hz",
-    position = "-1920x0@100.00Hz",
-    scale = "1"
+	output = "HDMI-A-1",
+	mode = "1920x1080@100.00Hz",
+	position = "-1920x0@100.00Hz",
+	scale = "1",
 })
 
 ----- [[ Misc configs ]] -----
 -- Keyboard layout
 hl.config({
-    input = {
-        kb_layout = "br",
-        kb_variant = "abnt2",
-        accel_profile = "flat",
-        tablet = {
-            transform = 1 -- 90 degrees clockwise
-        }
-    },
-    general = {
-        allow_tearing = true
-    }
+	input = {
+		kb_layout = "br",
+		kb_variant = "abnt2",
+		accel_profile = "flat",
+		tablet = {
+			transform = 1, -- 90 degrees clockwise
+		},
+	},
+	general = {
+		allow_tearing = true,
+	},
+})
+
+hl.config({
+	misc = {
+		vrr = 2,
+	},
 })
 
 -- Overwrite original config's blur
 hl.config({
-    decoration = {
-        blur = {
-            enabled = true,
-            xray = true,
-            special = false,
-            new_optimizations = true,
-            size = 10,
-            passes = 3,
-            brightness = 1,
-            noise = 0.0117,
-            contrast = 0.8916,
-            vibrancy = 0.1696,
-            vibrancy_darkness = 0.5,
-            popups = false,
-            popups_ignorealpha = 0.6,
-            input_methods = true,
-            input_methods_ignorealpha = 0.8
-        }
-    }
+	decoration = {
+		blur = {
+			enabled = true,
+			xray = true,
+			special = false,
+			new_optimizations = true,
+			size = 10,
+			passes = 3,
+			brightness = 1,
+			noise = 0.0117,
+			contrast = 0.8916,
+			vibrancy = 0.1696,
+			vibrancy_darkness = 0.5,
+			popups = false,
+			popups_ignorealpha = 0.6,
+			input_methods = true,
+			input_methods_ignorealpha = 0.8,
+		},
+	},
 })
